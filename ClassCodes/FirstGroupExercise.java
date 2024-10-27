82f8bd112cdcd84dd0b049519b40b9b17b2377e1
Angelica Ferriol <angelicaferriol712@gmail.com>
2024-10-27 23:29:15 +0800

    Task 1 & 2

--- ClassCodes/FirstGroupExercise.java
+++ ClassCodes/FirstGroupExercise.java
@@ -7,7 +7,7 @@ class StudentProfile {
     int year;
     String section;
     
-    // Create a constructor
+    // Constructor to initialize the fields
     StudentProfile(String name, String program, int year, String section) {
         this.name = name;
         this.program = program;
@@ -15,7 +15,7 @@ class StudentProfile {
         this.section = section;
     }
 
-    // Display Student Profile method
+    // Method to display the student's profile
     void displayStudentProfile() {
         System.out.println("Name: " + name);
         System.out.println("Program: " + program);
@@ -23,7 +23,7 @@ class StudentProfile {
         System.out.println("Section: " + section + "\n");
     }
 
-    // Update Program method
+    // Method to update the student's program, year, and section
     void update(String newProgram, int newYear, String newSection) {
         this.program = newProgram;
         this.year = newYear;
@@ -38,19 +38,19 @@ public class FirstGroupExercise {
        
         System.out.println("THREE AMAZING STUDENTS' PROFILE\n");
 
-        // Instantiate 
+        // Instantiate student profiles 
         StudentProfile StudentProfile1 = new StudentProfile("Angelica Ferriol", "BS Political Science", 1, "1-5");
         StudentProfile StudentProfile2 = new StudentProfile("Kurt Viray", "BS Information Technology", 1, "1-1");
         StudentProfile StudentProfile3 = new StudentProfile("Railley Nieles", "BS Aerospace Engineering", 3, "3-6");
 
         System.out.println("ACADEMIC YEAR 2023-2024");
        
-        // Call the display method
+        // Display initial student profiles
         StudentProfile1.displayStudentProfile();
         StudentProfile2.displayStudentProfile();
         StudentProfile3.displayStudentProfile();
 
-        // Call the update method
+        // Update student profiles
         StudentProfile1.update("BS Computer Science", 2, "2-1N");
         StudentProfile2.update("BS Nursing", 2, "2-3");
         StudentProfile3.update("BS Psychology", 4, "4-5");
@@ -59,7 +59,7 @@ public class FirstGroupExercise {
     
         System.out.println("\nACADEMIC YEAR 2024-2025");
 
-        // Call the display method after updating
+        // Display updated student profiles
         StudentProfile1.displayStudentProfile();
         StudentProfile2.displayStudentProfile();
         StudentProfile3.displayStudentProfile();
